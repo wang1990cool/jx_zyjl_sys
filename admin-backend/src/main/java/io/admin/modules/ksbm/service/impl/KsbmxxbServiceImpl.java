42962a65ed18101a95c5d7ede89a53f5aa636aff
unknown <samsunsgh@163.com>
2019-03-19 00:28:10 +0800

    查询

--- admin-backend/src/main/java/io/admin/modules/ksbm/service/impl/KsbmxxbServiceImpl.java
+++ admin-backend/src/main/java/io/admin/modules/ksbm/service/impl/KsbmxxbServiceImpl.java
@@ -1,5 +1,6 @@
 package io.admin.modules.ksbm.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
@@ -18,9 +19,16 @@ public class KsbmxxbServiceImpl extends ServiceImpl<KsbmxxbDao, KsbmxxbEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String xsxh = (String)params.get("xsxh");
+        String ksnd=(String)params.get("ksnd");
+        String szbj=(String)params.get("szbj");
+
         Page<KsbmxxbEntity> page = this.selectPage(
                 new Query<KsbmxxbEntity>(params).getPage(),
-                new EntityWrapper<KsbmxxbEntity>()
+                new EntityWrapper<KsbmxxbEntity>().
+                        like(StringUtils.isNotBlank(xsxh), "xsxh", xsxh).
+                        like(StringUtils.isNotBlank(ksnd), "ksnd", ksnd).
+                        like(StringUtils.isNotBlank(szbj), "szbj", szbj)
         );
 
         return new PageUtils(page);
